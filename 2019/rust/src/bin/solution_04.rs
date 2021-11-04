6a35598f44223ce8ba8861aec5be0e779347bff0
jurisk <jurisk@users.noreply.github.com>
2021-11-04 07:38:00 +0200

    2019 Day 04

---
+++ 2019/rust/src/bin/solution_04.rs
@@ -0,0 +1,58 @@
+use itertools::Itertools;
+use std::ops::RangeInclusive;
+
+type Number = u32;
+
+fn to_digits(x: Number) -> Vec<u8> {
+    x.to_string()
+        .chars()
+        .map(|x| x.to_string().parse::<u8>().unwrap())
+        .collect()
+}
+
+fn never_decrease(digits: &[u8]) -> bool {
+    let mut copy = digits.to_owned();
+    copy.sort_unstable(); // not the most efficient, but really simple
+    copy == *digits
+}
+
+fn group(digits: &[u8]) -> Vec<u8> {
+    digits
+        .iter()
+        .into_group_map_by(|x| *x)
+        .iter()
+        .map(|(_k, v)| v.len() as u8)
+        .collect()
+}
+
+fn is_valid(x: Number, f: fn(Vec<u8>) -> bool) -> bool {
+    let digits = to_digits(x);
+    f(group(&digits)) && never_decrease(&digits)
+}
+
+fn is_valid_1(x: Number) -> bool {
+    is_valid(x, |grouped| grouped.iter().any(|x| *x >= 2))
+}
+
+fn is_valid_2(x: Number) -> bool {
+    is_valid(x, |grouped| grouped.contains(&2))
+}
+
+fn main() {
+    assert!(is_valid_1(111111));
+    assert!(!is_valid_1(223450));
+    assert!(!is_valid_1(123789));
+
+    let range: RangeInclusive<Number> = 387638..=919123;
+    let part_1 = range.clone().filter(|x| is_valid_1(*x)).count();
+    assert_eq!(part_1, 466);
+    println!("Part 1: {}", part_1);
+
+    assert!(is_valid_2(112233));
+    assert!(!is_valid_2(123444));
+    assert!(is_valid_2(111122));
+
+    let part_2 = range.filter(|x| is_valid_2(*x)).count();
+    assert_eq!(part_2, 292);
+    println!("Part 2: {}", part_2);
+}
