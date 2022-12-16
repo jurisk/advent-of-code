c2b97adc38bacb14a95f82a67138597c3e396312
Juris <jurisk@users.noreply.github.com>
2022-12-16 17:08:05 +0200

    2022, Day 16, Part 1 (WIP)

--- scala2/src/main/scala/jurisk/adventofcode/y2022/Advent16.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2022/Advent16.scala
@@ -10,8 +10,8 @@ import scala.collection.immutable.BitSet
 import scala.collection.mutable
 
 object Advent16 {
-  type Parsed = ValveDefinitions
-  type ValveId = Int
+  type Parsed     = ValveDefinitions
+  type ValveId    = Int
   type ValveIdSet = BitSet
 
   final case class ValveDefinition(
@@ -22,70 +22,42 @@ object Advent16 {
     leadsTo: ValveIdSet,
   )
 
-  final case class State1(
-    timeElapsed: Int,
-    waterReleased: Int,
-    location: ValveId,
-    valvesOpen: ValveIdSet,
-  ) {
-    def currentWaterRate(definitions: ValveDefinitions): Int = {
-      valvesOpen.map { v =>
-        definitions(v).flowRate
-      }.sum
-    }
-
-    def isFinished(maxTime: Int): Boolean = {
-      timeElapsed > maxTime
-    }
-
-    def debug(definitions: ValveDefinitions): Unit = {
-      println(s"== Minute $timeElapsed ==")
-      if (valvesOpen.isEmpty) {
-        println("No valves are open.")
-      } else {
-        println(s"Valves ${valvesOpen.toList.sorted.mkString(", ")} are open, releasing ${currentWaterRate(definitions)} pressure.")
-      }
-      println(s"You are at $location and $waterReleased water has been released so far.")
-    }
-  }
-
   final case class State2(
     timeElapsed: Int,
     waterReleased: Int,
-    locations: ValveIdSet,
+    locations: List[
+      (ValveId, Option[ValveId]) // (location, destinationWithIntentionToOpen)
+    ],
     valvesOpen: ValveIdSet,
   ) {
-    def goFromBoth(definitions: ValveDefinitions, a: ValveId, b: ValveId): List[State2] = {
-      val neighboursA = definitions(a).leadsTo.toList
-      val neighboursB = definitions(b).leadsTo.toList
-      for {
-        an <- neighboursA
-        bn <- neighboursB
-      } yield State2(
-        timeElapsed,
-        waterReleased,
-        BitSet.fromSpecific(an :: bn :: Nil),
-        valvesOpen,
-      )
-    }
+    def variousOptions(
+      actorWithIntention: (ValveId, Option[ValveId]),
+      definition: TaskDefinition,
+    ): List[(ValveId, Option[ValveId], Option[ValveId])] = {
+      val (currentLocation, intention) = actorWithIntention
+      val result                       = intention match {
+        case Some(whereWeWantToGo) =>
+          if (whereWeWantToGo == currentLocation) { // we wanted to open this valve so we do it
+            (currentLocation, None, Some(currentLocation)) :: Nil
+          } else { // follow our intention
+            val nextStep =
+              definition.howDoWeGoTo((currentLocation, whereWeWantToGo))
+            // println(s"Next step of going from ${definition.nodeName(currentLocation)} to ${definition.nodeName(whereWeWantToGo)} is ${definition.nodeName(nextStep)}")
+            (nextStep, Some(whereWeWantToGo), None) :: Nil
+          }
 
-    def openValveAndGoFrom(definitions: ValveDefinitions, valveToOpen: ValveId, goFrom: ValveId): List[State2] = {
-      definitions(goFrom).leadsTo.toList.map { newLocation =>
-        State2(
-          timeElapsed,
-          waterReleased,
-          BitSet.fromSpecific(valveToOpen :: newLocation :: Nil),
-          valvesOpen + valveToOpen
-        )
+        case None =>
+          val valvesStillClosed: ValveIdSet =
+            definition.usefulValves -- valvesOpen
+          valvesStillClosed.toList map { whereWeWantToGo =>
+            val firstStep =
+              definition.howDoWeGoTo((currentLocation, whereWeWantToGo))
+            // println(s"Next step of going from ${definition.nodeName(currentLocation)} to ${definition.nodeName(whereWeWantToGo)} is ${definition.nodeName(firstStep)}")
+            (firstStep, Some(whereWeWantToGo), None)
+          }
       }
+      result
     }
-
-    def openTwoValves(a: ValveId, b: ValveId): State2 = State2(
-      timeElapsed,
-      waterReleased,
-      BitSet.fromSpecific(a :: b :: Nil),
-      valvesOpen + a + b,
-    )
 
     def rollTurn(definitions: ValveDefinitions): State2 =
       State2(
@@ -95,53 +67,52 @@ object Advent16 {
         valvesOpen,
       )
 
-    def currentWaterRate(definitions: ValveDefinitions): Int = {
+    def currentWaterRate(definitions: ValveDefinitions): Int =
       valvesOpen.map { v =>
         definitions(v).flowRate
       }.sum
-    }
 
-    def isFinished(maxTime: Int): Boolean = {
+    def isFinished(maxTime: Int): Boolean =
       timeElapsed > maxTime
-    }
 
-    val locationList: List[ValveId] = locations.toList.sorted match {
-      case x :: Nil => x :: x :: Nil
-      case a :: b :: Nil => a :: b :: Nil
-      case _ => sys.error("wtf")
-    }
-
-    def debug(definitions: ValveDefinitions): Unit = {
+    def debug(definition: TaskDefinition): Unit = {
       println(s"== Minute $timeElapsed ==")
       if (valvesOpen.isEmpty) {
         println("No valves are open.")
       } else {
-        println(s"Valves ${valvesOpen.toList.sorted.mkString(", ")} are open, releasing ${currentWaterRate(definitions)} pressure.")
+        println(
+          s"Valves ${valvesOpen.map(definition.nodeName).toList.sorted.mkString(", ")} are open, releasing ${currentWaterRate(definition.definitions)} pressure."
+        )
       }
-      println(s"You are at $locations and $waterReleased water has been released so far.")
+      println(s"You are at ${locations
+          .map { case (a, b) =>
+            definition.nodeName(a) + " intending for " + (b.map(definition.nodeName))
+          }
+          .mkString(", ")} and $waterReleased water has been released so far.")
     }
   }
 
   object ValveDefinition {
-    def parse(id: Int, s: String): ValveDefinition = {
+    def parse(id: Int, s: String): ValveDefinition =
       s match {
-        case s"Valve $idString has flow rate=$flowRate; tunnels lead to valves $others" => ValveDefinition(
-          id,
-          idString,
-          flowRate.toInt,
-          others.split(", ").toSet,
-          BitSet.empty,
-        )
-        case s"Valve $idString has flow rate=$flowRate; tunnel leads to valve $other" => ValveDefinition(
-          id,
-          idString,
-          flowRate.toInt,
-          Set(other),
-          BitSet.empty,
-        )
-        case _                 => s"Failed to parse $s".fail
+        case s"Valve $idString has flow rate=$flowRate; tunnels lead to valves $others" =>
+          ValveDefinition(
+            id,
+            idString,
+            flowRate.toInt,
+            others.split(", ").toSet,
+            BitSet.empty,
+          )
+        case s"Valve $idString has flow rate=$flowRate; tunnel leads to valve $other"   =>
+          ValveDefinition(
+            id,
+            idString,
+            flowRate.toInt,
+            Set(other),
+            BitSet.empty,
+          )
+        case _                                                                          => s"Failed to parse $s".fail
       }
-    }
   }
 
   type ValveDefinitions = Map[ValveId, ValveDefinition]
@@ -149,93 +120,93 @@ object Advent16 {
     definitions: ValveDefinitions,
     maxTime: Int,
   ) {
-    def successors1(state: State1): List[State1] = {
+    def nodeName(x: ValveId): String = definitions(x).idString
+
+    val howDoWeGoTo: Map[(ValveId, ValveId), ValveId] = {
+      val results: Iterable[(ValveId, ValveId, Option[ValveId])] = for {
+        from <- definitions.keys
+        to   <- definitions.keys
+        if from != to
+      } yield (
+        from,
+        to,
+        Bfs
+          .bfs(
+            from,
+            (n: ValveId) => definitions(n).leadsTo.toList,
+            (n: ValveId) => n == to,
+          )
+          .map(x => x.tail.head),
+      )
+
+      results.flatMap { case (from, to, q) =>
+        q map { q =>
+          (from, to) -> q
+        }
+      }.toMap
+    }
+
+    def addToBitSet(bitSet: ValveIdSet, opt: Option[ValveId]): ValveIdSet =
+      opt match {
+        case None    => bitSet
+        case Some(x) => bitSet + x
+      }
+
+    def successors2(state: State2): List[State2] =
       if (state.isFinished(maxTime)) {
         Nil
       } else {
-        if (definitions.keySet.size == state.valvesOpen.size) { // all valves open
-          State1(
-            state.timeElapsed + 1,
-            state.waterReleased + state.currentWaterRate(definitions),
-            state.location,
-            state.valvesOpen,
-          ) :: Nil
+        if (state.valvesOpen.size == usefulValves.size) { // all valves open
+          state.rollTurn(definitions) :: Nil
         } else {
-          val maybeOpen: Option[State1] = if ((definitions(state.location).flowRate > 0) && !state.valvesOpen(state.location)) {
-            State1(
-              state.timeElapsed + 1,
-              state.waterReleased + state.currentWaterRate(definitions),
-              state.location,
-              state.valvesOpen + state.location
-            ).some
-          } else {
-            none
-          }
+          val rolled    = state.rollTurn(definitions)
+          val locations = state.locations
 
-          val whereCanGo: List[State1] = definitions(state.location).leadsTo.toList.map { x =>
-            State1(
-              state.timeElapsed + 1,
-              state.waterReleased + state.currentWaterRate(definitions),
-              x,
-              state.valvesOpen,
-            )
-          }
+          locations match {
+            case x :: Nil      =>
+              rolled.variousOptions(x, this).map {
+                opt: (ValveId, Option[ValveId], Option[ValveId]) =>
+                  val (a1, a2, a3) = opt
+                  rolled
+                    .copy(locations = (a1, a2) :: Nil)
+                    .copy(valvesOpen = addToBitSet(rolled.valvesOpen, a3))
+              }
+            case a :: b :: Nil =>
+              val optionsForA = rolled.variousOptions(a, this)
+              val optionsForB = rolled.variousOptions(b, this)
 
-          val results = maybeOpen.toList ::: whereCanGo
-          results
+              // TODO: I think we can have conflicting stuff here, perhaps if both try to open the same valve then let us skip?
+              optionsForA.flatMap { ao =>
+                val (ao1, ao2, ao3) = ao
+                optionsForB.flatMap { bo =>
+                  val (bo1, bo2, bo3) = bo
+                  if (ao3.isDefined && ao3 == bo3) {
+                    none
+                  } else {
+                    rolled
+                      .copy(locations = (ao1, ao2) :: (bo1, bo2) :: Nil)
+                      .copy(valvesOpen =
+                        addToBitSet(addToBitSet(rolled.valvesOpen, ao3), bo3)
+                      )
+                      .some
+                  }
+                }
+              }
+
+            case _ => sys.error("wtf")
+          }
         }
       }
-    }
 
-    def successors2(state: State2): List[State2] = {
-      if (state.isFinished(maxTime)) {
-        Nil
-      } else {
-        if (definitions.keySet.size == state.valvesOpen.size) { // all valves open
-          State2(
-            state.timeElapsed + 1,
-            state.waterReleased + state.currentWaterRate(definitions),
-            state.locations,
-            state.valvesOpen,
-          ) :: Nil
-        } else {
-          val List(a, b) = state.locationList
-          val usefulValveAtA = (definitions(a).flowRate > 0) && !state.valvesOpen(a)
-          val usefulValveAtB = (definitions(b).flowRate > 0) && !state.valvesOpen(b)
-
-          val first: List[State2] = if (usefulValveAtA && usefulValveAtB) {
-            state.rollTurn(definitions).openTwoValves(a, b) :: Nil
-          } else Nil
-
-          val second1: List[State2] = if (usefulValveAtA) {
-            state.rollTurn(definitions).openValveAndGoFrom(definitions, a, b)
-          } else Nil
-
-          val second2 = if (usefulValveAtB) {
-            state.rollTurn(definitions).openValveAndGoFrom(definitions, b, a)
-          } else Nil
-
-          val third: List[State2] = state.rollTurn(definitions).goFromBoth(definitions, a, b)
-
-          (first ::: second1 ::: second2 ::: third).distinct.sortBy(_.valvesOpen.size)(Ordering[Int].reverse)
-        }
-      }
-    }
-
+    val usefulValves: ValveIdSet =
+      BitSet.fromSpecific(definitions.values.filter(_.flowRate > 0).map(_.id))
   }
 
-  // (time, location, openValves)
-  type Key1 = (Int, ValveId, ValveIdSet)
-  var bestCache1: mutable.Map[Key1, State1] = mutable.Map.empty
-
-  // (time, locations, openValves)
-  type Key2 = (Int, ValveIdSet, ValveIdSet)
-  var bestCache2: mutable.Map[Key2, State2] = mutable.Map.empty
-
   def parse(data: String): Parsed = {
-    val input = data.split("\n")
-    val parsed: Array[ValveDefinition] = input.zipWithIndex.map { case (s, idx) =>
-      ValveDefinition.parse(idx, s)
+    val input                          = data.split("\n")
+    val parsed: Array[ValveDefinition] = input.zipWithIndex.map {
+      case (s, idx) =>
+        ValveDefinition.parse(idx, s)
     }
 
     parsed.toList.map { x =>
@@ -250,81 +221,71 @@ object Advent16 {
   def part1(definitions: Parsed): Int = {
     val startValve = definitions.values.find(_.idString == "AA").get.id
 
-    println(definitions)
-    val start = State1(
+    val start = State2(
       1,
       0,
-      startValve,
+      (startValve, None) :: Nil,
       BitSet.empty,
     )
 
-    var best: Int = 0
-    def visit(state: State1): Unit = {
-      if (state.waterReleased > best) {
-        best = state.waterReleased
-      }
-    }
-
-    val taskDefinition = TaskDefinition(definitions, 30)
-
-    def successors(state: State1): List[State1] = {
-      val key = (state.timeElapsed, state.location, state.valvesOpen)
-      val bestSoFar = bestCache1.get(key)
-      bestSoFar match {
-        case Some(x) =>
-          if (x.waterReleased > state.waterReleased) {
-            Nil
-          } else {
-            bestCache1.update(key, state)
-            taskDefinition.successors1(state)
-          }
-
-        case None =>
-          bestCache1.update(key, state)
-          taskDefinition.successors1(state)
-      }
-
-    }
-
-    Bfs.bfsVisitAll(start, successors, visit)
-
-    best
+    solve(definitions, start, 30)
   }
 
   def part2(definitions: Parsed): Int = {
-    println(definitions)
     val startValve = definitions.values.find(_.idString == "AA").get.id
 
     val start = State2(
       1,
       0,
-      BitSet.fromSpecific(startValve :: Nil),
+      (startValve, None) :: (startValve, None) :: Nil,
       BitSet.empty,
     )
 
+    solve(definitions, start, 26)
+  }
+
+  def solve(definitions: Parsed, start: State2, maxTime: Int): Int = {
+    // println(definitions)
+
     var best: Int = 0
 
-    def visit(state: State2): Unit = {
+    def visit(state: State2): Unit =
       if (state.waterReleased > best) {
         best = state.waterReleased
         println(s"Found better $best at $state")
       }
-    }
 
-    val taskDefinition = TaskDefinition(definitions, 26)
+    val taskDefinition = TaskDefinition(definitions, maxTime)
+
+    // (time, locations, openValves)
+    type Key2 = (Int, ValveIdSet, ValveIdSet)
+    val bestCache2: mutable.Map[Key2, State2] = mutable.Map.empty
 
     def successors(state: State2): List[State2] = {
-      val key: Key2 = (state.timeElapsed, state.locations, state.valvesOpen)
-      val bestAtThisSituation = bestCache2.get(key).map(_.waterReleased).getOrElse(0)
+      val key: Key2           = (
+        state.timeElapsed,
+        BitSet.fromSpecific(state.locations.map(_._1)),
+        state.valvesOpen,
+      )
+      val bestAtThisSituation =
+        bestCache2.get(key).map(_.waterReleased).getOrElse(0)
 
       if (bestAtThisSituation > state.waterReleased) {
         Nil // prune
       } else {
-        if (bestCache2.size % 10000 == 0) {
+        if (bestCache2.size % 100000 == 0) {
           println(bestCache2.size)
         }
         bestCache2.update(key, state)
-        taskDefinition.successors2(state)
+        val results = taskDefinition.successors2(state)
+//        println(s"From $state => ")
+//        state.debug(taskDefinition)
+//        println(" potential following options exist =>")
+//        results.foreach { x =>
+//          x.debug(taskDefinition)
+//        }
+//        println
+        results
       }
     }
 
@@ -343,7 +304,6 @@ object Advent16 {
 
   def main(args: Array[String]): Unit = {
     val testData = readFileText("2022/16-test.txt")
-    // val testData = """"""
     val realData = readFileText("2022/16.txt")
 
     val test = parse(testData)
@@ -351,10 +311,10 @@ object Advent16 {
 
     println(real.keySet)
 
-//    part1(test) shouldEqual 1651
-//    part1(real) shouldEqual 1701
-//
+    part1(test) shouldEqual 1651
+    part1(real) shouldEqual 1701
+
     part2(test) shouldEqual 1707
-    part2(real) shouldEqual 1234465
+    part2(real) shouldEqual 12345678
   }
 }
