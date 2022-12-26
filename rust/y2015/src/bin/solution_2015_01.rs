51857370d1fdee31bff0779a978eeb4f0ebec95b
Juris <jurisk@users.noreply.github.com>
2022-12-26 19:56:32 +0200

    Minor changes

--- rust/y2015/src/bin/solution_2015_01.rs
+++ rust/y2015/src/bin/solution_2015_01.rs
@@ -1,10 +1,12 @@
 use advent_of_code_common::parsing::Error;
 use advent_of_code_common::utils::head_tail;
+use itertools::FoldWhile::{Continue, Done};
+use itertools::Itertools;
 use num_enum::TryFromPrimitive;
 
 const DATA: &str = include_str!("../../resources/01.txt");
 
-#[derive(Copy, Clone, TryFromPrimitive)]
+#[derive(Copy, Clone, TryFromPrimitive, Debug)]
 #[repr(u8)]
 enum Command {
     Up = b'(',
@@ -36,43 +38,24 @@ fn solve_1(data: &Data) -> Output1 {
     data.iter().map(|c| c.diff()).sum()
 }
 
-fn scan_finding_index_which_satisfies_predicate<S, P, State, Elem>(
-    remaining: &[Elem],
-    state: State,
-    index: usize,
-    next_state: S,
-    predicate: P,
-) -> Option<usize>
-where
-    S: Fn(State, &Elem) -> State,
-    P: Fn(&State) -> bool,
-{
-    if predicate(&state) {
-        Some(index)
-    } else {
-        let (h, t) = head_tail(remaining);
-        match h {
-            None => None,
-            Some(h) => scan_finding_index_which_satisfies_predicate(
-                t,
-                next_state(state, h),
-                index + 1,
-                next_state,
-                predicate,
-            ),
-        }
-    }
-}
-
 fn solve_2(data: &Data) -> Output2 {
-    scan_finding_index_which_satisfies_predicate(
-        data,
-        0,
-        0,
-        |floor, c| floor + c.diff(),
-        |x| *x < 0,
-    )
-    .unwrap_or_else(|| panic!("Not found"))
+    let (index, result) = data
+        .iter()
+        .fold_while((0, 0), |(idx, floor), b| {
+            let new_floor = floor + b.diff();
+            if new_floor < 0 {
+                Done((idx + 1, new_floor))
+            } else {
+                Continue((idx + 1, new_floor))
+            }
+        })
+        .into_inner();
+
+    if result < 0 {
+        index
+    } else {
+        panic!("Basement never reached in ${data:?}")
+    }
 }
 
 fn part_1(input: &str) -> Result<Output1, Error> {
