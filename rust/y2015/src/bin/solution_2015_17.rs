5698c5895f02b1285ef56c27074d1890e989381f
Juris <jurisk@users.noreply.github.com>
2023-01-07 21:48:36 +0200

    2015, Day 17, more efficient solution

--- rust/y2015/src/bin/solution_2015_17.rs
+++ rust/y2015/src/bin/solution_2015_17.rs
@@ -1,9 +1,13 @@
+use memoize::memoize;
+
 use advent_of_code_common::parsing::{parse_lines_to_vec, Error};
+use advent_of_code_common::utils::head_tail;
 
 fn parse(input: &str) -> Result<Vec<u16>, Error> {
     parse_lines_to_vec(input)
 }
 
+#[allow(dead_code)]
 fn select(data: &[u16], selection: u32) -> u16 {
     let mut current: u16 = 0;
 
@@ -16,6 +20,7 @@ fn select(data: &[u16], selection: u32) -> u16 {
     current
 }
 
+#[allow(dead_code)]
 fn valid_selections(data: &[u16], target: u16) -> Vec<u32> {
     let mut result = vec![];
 
@@ -28,12 +33,14 @@ fn valid_selections(data: &[u16], target: u16) -> Vec<u32> {
     result
 }
 
-fn solve_1(data: &[u16], target: u16) -> usize {
+#[allow(dead_code)]
+fn solve_1_old(data: &[u16], target: u16) -> usize {
     let selections = valid_selections(data, target);
     selections.len()
 }
 
-fn solve_2(data: &[u16], target: u16) -> usize {
+#[allow(dead_code)]
+fn solve_2_old(data: &[u16], target: u16) -> usize {
     let selections = valid_selections(data, target);
     let minimum_number_of_containers = selections.iter().map(|x| x.count_ones()).min().unwrap();
     selections
@@ -42,8 +49,63 @@ fn solve_2(data: &[u16], target: u16) -> usize {
         .count()
 }
 
+#[memoize]
+#[allow(clippy::needless_pass_by_value)]
+fn solve_1(data: Vec<u16>, target: u16) -> usize {
+    if target == 0 {
+        1
+    } else {
+        let (h, t) = head_tail(&data);
+        match h {
+            None => 0,
+            Some(&h) => {
+                let using_h = if target < h {
+                    0
+                } else {
+                    solve_1(t.to_vec(), target - h)
+                };
+                let not_using_h = solve_1(t.to_vec(), target);
+                using_h + not_using_h
+            },
+        }
+    }
+}
+
+#[memoize]
+#[allow(clippy::needless_pass_by_value)]
+fn solve_2_int(data: Vec<u16>, target: u16, bottles_remaining: u16) -> usize {
+    if target == 0 && bottles_remaining == 0 {
+        1
+    } else {
+        let (h, t) = head_tail(&data);
+        match h {
+            None => 0,
+            Some(&h) => {
+                let using_h = if target < h || bottles_remaining == 0 {
+                    0
+                } else {
+                    solve_2_int(t.to_vec(), target - h, bottles_remaining - 1)
+                };
+                let not_using_h = solve_2_int(t.to_vec(), target, bottles_remaining);
+                using_h + not_using_h
+            },
+        }
+    }
+}
+
+fn solve_2(data: &[u16], target: u16) -> usize {
+    for i in 1..=data.len() {
+        let result = solve_2_int(data.to_vec(), target, i.try_into().unwrap());
+        if result > 0 {
+            return result;
+        }
+    }
+
+    panic!("Not found")
+}
+
 fn part_1(input: &str, target: u16) -> Result<usize, Error> {
-    parse(input).map(|input| solve_1(&input, target))
+    parse(input).map(|input| solve_1(input, target))
 }
 
 fn part_2(input: &str, target: u16) -> Result<usize, Error> {
