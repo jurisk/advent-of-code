e2759e17d6522e3be572ac69ea5da8e3123bee6e
Juris <jurisk@users.noreply.github.com>
2023-08-20 06:12:37 +0300

    2017, Day 25 - WIP

---
+++ swift/ReadMe.md
@@ -0,0 +1,4 @@
+```shell
+swift run
+swift test
+```
---
+++ swift/Tests/2017/Day25/SolutionTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+@testable import AdventOfCode
+
+final class SolutionTests: XCTestCase {
+    func testPart1() throws {
+        XCTAssertEqual(part1("test"), 123)
+    }
+}
