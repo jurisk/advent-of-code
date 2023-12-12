0759122e527b687f774f5510331c0626e014d8fd
Juris <jurisk@users.noreply.github.com>
2023-12-12 08:07:26 +0200

    2023-12 WIP

---
+++ scala2/src/main/scala/jurisk/adventofcode/y2023/Advent12.scala
@@ -0,0 +1,166 @@
+package jurisk.adventofcode.y2023
+
+import jurisk.adventofcode.y2023.Advent12.Condition.{
+  Damaged,
+  Operational,
+  Unknown,
+}
+import jurisk.utils.FileInput._
+import jurisk.utils.Parsing.StringOps
+
+object Advent12 {
+  type Input = List[Row]
+
+  sealed trait Condition {
+    def symbol: Char
+    override def toString: String = symbol.toString
+  }
+
+  sealed trait NonOperational extends Condition
+
+  object Condition {
+    case object Operational extends Condition                     {
+      override def symbol: Char = '.'
+    }
+    case object Damaged     extends Condition with NonOperational {
+      override def symbol: Char = '#'
+    }
+    case object Unknown     extends Condition with NonOperational {
+      override def symbol: Char = '?'
+    }
+  }
+
+  def splitBySeparator[T](l: List[T], sep: T): List[List[T]] =
+    (l.span(_ != sep) match {
+      case (hd, _ :: tl) => hd :: splitBySeparator(tl, sep)
+      case (hd, _)       => List(hd)
+    }).filter(_.nonEmpty)
+
+  def printL(l: List[Condition]): String =
+    l.map(_.symbol).mkString
+
+  def arr(springs: List[List[NonOperational]], groups: List[Int]): Int = {
+    val result = groups match {
+      case nextGroup :: otherGroups =>
+        springs match {
+          case nextSprings :: otherSprings =>
+            val nextDamaged = nextSprings.head == Damaged
+
+            val groupCanStartHere = nextSprings.length >= nextGroup
+            val validSpaceAfter   = nextSprings.lift(nextGroup) != Some(Damaged)
+
+            val mult1 = if (groupCanStartHere && validSpaceAfter) {
+              val updated = nextSprings.drop(nextGroup + 1)
+              val rem     = if (updated.nonEmpty) {
+                updated :: otherSprings
+              } else {
+                otherSprings
+              }
+
+              arr(rem, otherGroups)
+            } else {
+              0
+            }
+
+            val mult2 = if (nextDamaged) { 0 }
+            else {
+              val updated = nextSprings.drop(1)
+              val rem     = if (updated.nonEmpty) {
+                updated :: otherSprings
+              } else {
+                otherSprings
+              }
+
+              arr(rem, nextGroup :: otherGroups)
+            }
+
+            mult1 + mult2
+
+          case Nil => 0 // Group left but no matching springs
+        }
+
+      case Nil =>
+        if (springs.forall(_.forall(_ == Unknown))) {
+          // some springs left, but no groups left, but all those springs can be empty
+          1
+        } else {
+          // we have damaged springs remaining but no groups left to cover
+          0
+        }
+    }
+
+//    println(s"`${springs.map(x => printL(x)).mkString(" ")}` $groups => $result")
+
+    result
+  }
+
+  final case class Row(
+    conditions: List[Condition],
+    groups: List[Int],
+  ) {
+    def expand(times: Int): Row = {
+      Row.parse(
+        List.fill(times)(printL(conditions)).mkString("?") + " " + List.fill(times)(groups.map(_.toString).mkString(",")).mkString(",")
+      )
+    }
+
+    def expandedArrangements(times: Int): Int = {
+      expand(times).arrangements
+    }
+
+    def arrangements: Int = {
+      val grouped: List[List[NonOperational]] =
+        splitBySeparator(conditions, Operational).map { list =>
+          list.map {
+            case x: NonOperational => x
+            case x                 => x.toString.fail
+          }
+        }
+//
+//      println(conditions)
+//      println(grouped)
+//      println(groups)
+//      println()
+
+      arr(grouped, groups)
+    }
+  }
+
+  object Row {
+    def parse(s: String): Row = {
+      println(s)
+
+      val (a, b)     = s.splitPairUnsafe(" ")
+      val conditions = a.toList.map {
+        case '.' => Operational
+        case '#' => Damaged
+        case '?' => Unknown
+        case ch  => ch.toString.fail
+      }
+
+      val groups = b.extractIntList
+
+      Row(conditions, groups)
+    }
+
+  }
+
+  def parse(input: String): Input =
+    input.parseLines(Row.parse)
+
+  def part1(data: Input): Int =
+    data.map(_.arrangements).sum
+
+  def part2(data: Input): Int =
+    data.map(_.expandedArrangements(5)).sum
+
+  def parseFile(fileName: String): Input =
+    parse(readFileText(fileName))
+
+  def main(args: Array[String]): Unit = {
+    val realData: Input = parseFile("2023/12.txt")
+
+    println(s"Part 1: ${part1(realData)}")
+    println(s"Part 2: ${part2(realData)}")
+  }
+}
---
+++ scala2/src/test/scala/jurisk/adventofcode/y2023/Advent12Spec.scala
@@ -0,0 +1,87 @@
+package jurisk.adventofcode.y2023
+
+import org.scalatest.freespec.AnyFreeSpec
+import Advent12._
+import org.scalatest.matchers.should.Matchers._
+
+class Advent12Spec extends AnyFreeSpec {
+  "arrangements" - {
+    "a" in {
+      Row.parse("# 1").arrangements shouldEqual 1
+      Row.parse("? 1").arrangements shouldEqual 1
+      Row.parse(". 1").arrangements shouldEqual 0
+    }
+
+    "aa" in {
+      Row.parse("# 2").arrangements shouldEqual 0
+    }
+
+    "b" in {
+      Row.parse("?? 1").arrangements shouldEqual 2
+    }
+
+    "c" in {
+      Row.parse("??? 1,1").arrangements shouldEqual 1
+    }
+
+    "1" in {
+      Row.parse("???.### 1,1,3").arrangements shouldEqual 1
+    }
+
+    "1 expand" in {
+      Row.parse("???.### 1,1,3").expandedArrangements(5) shouldEqual 1
+    }
+
+    "2" in {
+      Row.parse(".??..??...?##. 1,1,3").arrangements shouldEqual 4
+    }
+
+    "3" in {
+      Row.parse("?#?#?#?#?#?#?#? 1,3,1,6").arrangements shouldEqual 1
+    }
+
+    "4" in {
+      Row.parse("????.#...#... 4,1,1").arrangements shouldEqual 1
+    }
+
+    "5" in {
+      Row.parse("????.######..#####. 1,6,5").arrangements shouldEqual 4
+    }
+
+    "6a" in {
+      Row.parse("??????? 2,1").arrangements shouldEqual 10
+    }
+
+    "6b" in {
+      Row.parse("?###? 3").arrangements shouldEqual 1
+    }
+
+    "6c" in {
+      Row.parse("###? 3").arrangements shouldEqual 1
+    }
+
+    "6" in {
+      Row.parse("?###???????? 3,2,1").arrangements shouldEqual 10
+    }
+
+    "6 expand" in {
+      Row.parse("?###???????? 3,2,1").expandedArrangements(5) shouldEqual 506250
+    }
+  }
+
+  "part 1" - {
+    "real" in {
+      part1(parseFile("2023/12.txt")) shouldEqual 7694
+    }
+  }
+
+  "part 2" - {
+//    "test" in {
+//      part2(parseFile("2023/12-test.txt")) shouldEqual 0
+//    }
+
+    "real" in {
+      part2(parseFile("2023/12.txt")) shouldEqual 0
+    }
+  }
+}
