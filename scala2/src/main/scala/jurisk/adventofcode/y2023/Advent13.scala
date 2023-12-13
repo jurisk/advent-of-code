e516e937198057a6614b72784e33b6926ba1a9bc
Juris <jurisk@users.noreply.github.com>
2023-12-13 09:25:39 +0200

    2023-12 - refactoring

--- scala2/src/main/scala/jurisk/adventofcode/y2023/Advent13.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2023/Advent13.scala
@@ -10,24 +10,44 @@ import mouse.all._
 object Advent13 {
   type Input = List[Field2D[Boolean]]
 
+  sealed trait Reflection {
+    def covered: Int
+    def value: Int
+  }
+
+  private object Reflection {
+    final case class Horizontal(skipTop: Int, covered: Int) extends Reflection {
+      def value: Int = skipTop * 100
+    }
+    final case class Vertical(skipLeft: Int, covered: Int)  extends Reflection {
+      def value: Int = skipLeft
+    }
+  }
+
   def parse(input: String): Input =
     input.splitByDoubleNewline.map(s => Field2D.parseBooleanField(s))
 
-  private def verticalReflections(field: Field2D[Boolean]): List[Int] =
-    horizontalReflections(field.rotate(Rotation.Left90))
+  private def verticalReflections(
+    field: Field2D[Boolean]
+  ): List[Reflection.Vertical] =
+    horizontalReflections(field.rotate(Rotation.Left90)).map { reflection =>
+      Reflection.Vertical(reflection.skipTop, reflection.covered)
+    }
 
   private def horizontalReflectionPerfect(
     field: Field2D[Boolean]
-  ): Option[Int] =
+  ): Option[Reflection.Horizontal] =
     if (field.height % 2 == 0) {
       val half = field.height / 2
       val a    = field.topRows(half)
       val b    = field.bottomRows(half).reverseRows
 
-      (a == b).option(half)
+      (a == b).option(Reflection.Horizontal(half, field.height))
     } else none
 
-  private def horizontalReflections(field: Field2D[Boolean]): List[Int] = {
+  private def horizontalReflections(
+    field: Field2D[Boolean]
+  ): List[Reflection.Horizontal] = {
     val maxSkip = field.height - 2
 
     val optionsDropBottom = (0 to maxSkip).map { drop =>
@@ -38,20 +58,18 @@ object Advent13 {
     // Starts from 1 on purpose so we don't do unchanged "field" twice
     val optionsDropTop = (1 to maxSkip).map { drop =>
       val newField = field.bottomRows(field.height - drop)
-      horizontalReflectionPerfect(newField).map(_ + drop)
+      horizontalReflectionPerfect(newField).map(result =>
+        result.copy(skipTop = result.skipTop + drop)
+      )
     }
 
     (optionsDropBottom.toList ::: optionsDropTop.toList).flatten.sortBy(x =>
-      (x - field.height / 2).abs
-    )
+      x.covered
+    )(Ordering[Int].reverse)
   }
 
-  private def values(field: Field2D[Boolean]): List[Int] = {
-    val v = verticalReflections(field)
-    val h = horizontalReflections(field).map(x => x * 100)
-
-    v ::: h
-  }
+  private def values(field: Field2D[Boolean]): List[Int] =
+    (verticalReflections(field) ::: horizontalReflections(field)).map(_.value)
 
   def initialValue(field: Field2D[Boolean]): Int =
     values(field).singleResultUnsafe
