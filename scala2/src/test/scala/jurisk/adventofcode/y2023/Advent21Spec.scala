e906355a55b0130390d1a24f32c5a758a4036964
Juris <jurisk@users.noreply.github.com>
2023-12-21 05:11:23 +0200

    2023-21 - template

---
+++ scala2/src/test/scala/jurisk/adventofcode/y2023/Advent21Spec.scala
@@ -0,0 +1,30 @@
+package jurisk.adventofcode.y2023
+
+import org.scalatest.freespec.AnyFreeSpec
+import Advent21._
+import org.scalatest.matchers.should.Matchers._
+
+class Advent21Spec extends AnyFreeSpec {
+  private def testData = parseFile(fileName("-test"))
+  private def realData = parseFile(fileName(""))
+
+  "part 1" - {
+    "test" in {
+      part1(testData) shouldEqual 0
+    }
+
+    "real" in {
+      part1(realData) shouldEqual 0
+    }
+  }
+
+  "part 2" - {
+    "test" in {
+      part2(testData) shouldEqual 0
+    }
+
+    "real" in {
+      part2(realData) shouldEqual 0
+    }
+  }
+}
