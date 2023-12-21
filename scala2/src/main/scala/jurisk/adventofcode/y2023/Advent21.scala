8a3d1d308ffc785f17a10ab1da749517839a23e9
Juris <jurisk@users.noreply.github.com>
2023-12-21 07:34:42 +0200

    2023-21 - WIP

--- scala2/src/main/scala/jurisk/adventofcode/y2023/Advent21.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2023/Advent21.scala
@@ -1,10 +1,11 @@
 package jurisk.adventofcode.y2023
 
 import cats.implicits.toFunctorOps
-import jurisk.geometry.{Coords2D, Field2D}
+import jurisk.geometry.Coords2D
+import jurisk.geometry.Field2D
+import jurisk.math.absForWrappingAround
 import jurisk.utils.CollectionOps.IterableOps
 import jurisk.utils.FileInput._
-import jurisk.utils.Parsing.StringOps
 import jurisk.utils.Simulation
 
 object Advent21 {
@@ -14,7 +15,7 @@ object Advent21 {
   )
 
   def parse(input: String): Input = {
-    val temp = Field2D.parseCharField(input)
+    val temp  = Field2D.parseCharField(input)
     val start = temp.filterCoordsByValue(_ == 'S').singleResultUnsafe
 
     val field = temp.map {
@@ -31,16 +32,36 @@ object Advent21 {
 
   def part1(data: Input, steps: Int): Int = {
     val positions = Set(data.start)
-    val results = Simulation.runNIterations(positions, steps) { case (current, counter) =>
-      current flatMap { c =>
-        data.field.adjacent4Where(c, _ == false)
-      }
+    val results   = Simulation.runNIterations(positions, steps) {
+      case (current, counter) =>
+        current flatMap { c =>
+          data.field.adjacent4Where(c, _ == false)
+        }
     }
     results.size
   }
 
-  def part2(data: Input): Int =
-    0
+  def part2(data: Input, steps: Int): Long = {
+    val field                    = data.field
+    val positions: Set[Coords2D] = Set(data.start)
+    val results                  = Simulation.runNIterations(positions, steps) {
+      case (current, counter) =>
+        val options = current.toList.flatMap { c =>
+          val validNeighbours = c.adjacent4.filter { neighbour =>
+            val newX     = absForWrappingAround(neighbour.x, field.width)
+            val newY     = absForWrappingAround(neighbour.y, field.height)
+            val adjusted = Coords2D(newX, newY)
+            field.at(adjusted).contains(false)
+          }
+
+          validNeighbours
+        }
+
+        options.toSet
+    }
+
+    results.size
+  }
 
   def parseFile(fileName: String): Input =
     parse(readFileText(fileName))
@@ -52,6 +73,6 @@ object Advent21 {
     val realData: Input = parseFile(fileName(""))
 
     println(s"Part 1: ${part1(realData, 64)}")
-    println(s"Part 2: ${part2(realData)}")
+    println(s"Part 2: ${part2(realData, 26501365)}")
   }
 }
