dde15ee2166aee068bd9ce4522af0cbe71320d8c
Juris <jurisk@users.noreply.github.com>
2023-12-21 07:34:42 +0200

    2023-21 - WIP

--- scala2/src/test/scala/jurisk/adventofcode/y2023/Advent21Spec.scala
+++ scala2/src/test/scala/jurisk/adventofcode/y2023/Advent21Spec.scala
@@ -19,12 +19,36 @@ class Advent21Spec extends AnyFreeSpec {
   }
 
   "part 2" - {
-    "test" in {
-      part2(testData) shouldEqual 0
+    "test 6" in {
+      part2(testData, 6) shouldEqual 16
+    }
+
+    "test 10" in {
+      part2(testData, 10) shouldEqual 50
+    }
+
+    "test 50" in {
+      part2(testData, 50) shouldEqual 1594
+    }
+
+    "test 100" in {
+      part2(testData, 100) shouldEqual 6536
+    }
+
+    "test 500" in {
+      part2(testData, 500) shouldEqual 167004
+    }
+
+    "test 1000" in {
+      part2(testData, 1000) shouldEqual 668697
+    }
+
+    "test 5000" in {
+      part2(testData, 5000) shouldEqual 16733044
     }
 
     "real" in {
-      part2(realData) shouldEqual 0
+      part2(realData, 26501365) shouldEqual 0
     }
   }
 }
