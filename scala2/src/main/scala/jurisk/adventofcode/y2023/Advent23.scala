1f4dc824588a311821c893de128cf45f9c0443d3
Juris <jurisk@users.noreply.github.com>
2023-12-23 06:49:57 +0200

    2023-23 - WIP

---
+++ scala2/src/main/scala/jurisk/adventofcode/y2023/Advent23.scala
@@ -0,0 +1,47 @@
+package jurisk.adventofcode.y2023
+
+import jurisk.utils.FileInput._
+import jurisk.utils.Parsing.StringOps
+
+object Advent23 {
+  type Input = List[Command]
+
+  sealed trait Command extends Product with Serializable
+  object Command {
+    case object Noop                      extends Command
+    final case class Something(
+      values: List[Int]
+    ) extends Command
+    final case class Other(value: String) extends Command
+
+    def parse(s: String): Command =
+      s match {
+        case "noop"            => Noop
+        case s"something $rem" => Something(rem.extractIntList)
+        case s if s.nonEmpty   => Other(s)
+        case _                 => s.failedToParse
+      }
+  }
+
+  def parse(input: String): Input =
+    input.parseLines(Command.parse)
+
+  def part1(data: Input): Int =
+    0
+
+  def part2(data: Input): Int =
+    0
+
+  def parseFile(fileName: String): Input =
+    parse(readFileText(fileName))
+
+  def fileName(suffix: String): String =
+    s"2023/23$suffix.txt"
+
+  def main(args: Array[String]): Unit = {
+    val realData: Input = parseFile(fileName(""))
+
+    println(s"Part 1: ${part1(realData)}")
+    println(s"Part 2: ${part2(realData)}")
+  }
+}
