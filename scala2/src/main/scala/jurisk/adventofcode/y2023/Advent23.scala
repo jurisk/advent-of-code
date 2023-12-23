3504c776cf02320ab1520c2ab980c2b1ce51647c
Juris <jurisk@users.noreply.github.com>
2023-12-23 07:22:49 +0200

    2023-23 - WIP

--- scala2/src/main/scala/jurisk/adventofcode/y2023/Advent23.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2023/Advent23.scala
@@ -1,36 +1,94 @@
 package jurisk.adventofcode.y2023
 
+import cats.implicits.toFunctorOps
+import jurisk.adventofcode.y2023.Advent23.Square.{Forest, Path, Slope}
+import jurisk.algorithms.pathfinding.Bfs
+import jurisk.collections.BiMap
+import jurisk.collections.BiMap.BiDirectionalArrowAssociation
+import jurisk.geometry.Direction2D.CardinalDirection2D
+import jurisk.geometry.{Coords2D, Direction2D, Field2D}
 import jurisk.utils.FileInput._
 import jurisk.utils.Parsing.StringOps
 
 object Advent23 {
-  type Input = List[Command]
+  type Input = Field2D[Square]
 
-  sealed trait Command extends Product with Serializable
-  object Command {
-    case object Noop                      extends Command
-    final case class Something(
-      values: List[Int]
-    ) extends Command
-    final case class Other(value: String) extends Command
-
-    def parse(s: String): Command =
-      s match {
-        case "noop"            => Noop
-        case s"something $rem" => Something(rem.extractIntList)
-        case s if s.nonEmpty   => Other(s)
-        case _                 => s.failedToParse
-      }
+  sealed trait Square extends Product with Serializable
+  object Square {
+    case object Forest extends Square
+    case object Path extends Square
+    final case class Slope(direction: CardinalDirection2D) extends Square
   }
 
   def parse(input: String): Input =
-    input.parseLines(Command.parse)
+    Field2D.parseFromBiMap(input, BiMap(
+      '#' <-> Forest,
+      '.' <-> Path,
+      '>' <-> Slope(Direction2D.E),
+      'v' <-> Slope(Direction2D.S),
+      '^' <-> Slope(Direction2D.N),
+      '<' <-> Slope(Direction2D.W),
+    ))
 
-  def part1(data: Input): Int =
-    0
+  final case class State(
+    visited: Set[Coords2D],
+    steps: Int,
+    current: Coords2D,
+  ) {
+    def next(field: Input): List[State] = {
+      val candidates = field.atOrElse(current, Forest) match {
+        case Square.Forest => "wtf".fail
+        case Square.Path => field.adjacent4(current)
+        case Slope(direction) => {
+          val n = current + direction
+          if (field.isValidCoordinate(n))
+            n :: Nil
+          else
+            Nil
+        }
+      }
 
-  def part2(data: Input): Int =
-    0
+      val valid = candidates.filter(c => field.atOrElse(c, Forest) != Forest).filterNot(visited.contains)
+
+      valid map { c =>
+        State(
+          visited = visited + c,
+          steps = steps + 1,
+          current = c
+        )
+      }
+    }
+  }
+
+  def solve1(data: Input): Int = {
+    val startCoords = data.topRowCoords.find(c => data.at(c).contains(Path)).get
+    val goalCoords = data.bottomRowCoords.find(c => data.at(c).contains(Path)).get
+
+    val startState = State(
+      visited = Set(startCoords),
+      steps = 0,
+      current = startCoords,
+    )
+
+    val reachable = Bfs.bfsReachable[State](
+      startState,
+      _.next(data),
+    )
+
+    reachable.filter(_.current == goalCoords).maxBy(_.steps).steps
+  }
+
+  def part1(data: Input): Long = solve1(data)
+
+  def part2(data: Input): Long = {
+    val updated = data.map {
+      case Square.Forest => Square.Forest
+      case Square.Path => Square.Path
+      case Slope(_) => Square.Path
+    }
+
+    solve1(updated)
+  }
 
   def parseFile(fileName: String): Input =
     parse(readFileText(fileName))
