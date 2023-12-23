c3198e978bc3b83e8c918b80ad932f4b9bee5586
Juris <jurisk@users.noreply.github.com>
2023-12-23 10:21:49 +0200

    2023-23 - WIP

--- scala2/src/main/scala/jurisk/adventofcode/y2023/Advent23.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2023/Advent23.scala
@@ -2,16 +2,19 @@ package jurisk.adventofcode.y2023
 
 import cats.implicits.toFunctorOps
 import jurisk.adventofcode.y2023.Advent23.Square.{Forest, Path, Slope}
-import jurisk.algorithms.pathfinding.Bfs
-import jurisk.collections.BiMap
+import jurisk.algorithms.pathfinding.{Bfs, Dijkstra, FloydWarshall}
+import jurisk.collections.{BiMap, SetOfTwo}
 import jurisk.collections.BiMap.BiDirectionalArrowAssociation
 import jurisk.geometry.Direction2D.CardinalDirection2D
 import jurisk.geometry.{Coords2D, Direction2D, Field2D}
+import jurisk.utils.CollectionOps.IterableOps
 import jurisk.utils.FileInput._
 import jurisk.utils.Parsing.StringOps
 
+import scala.collection.immutable.ArraySeq
+
 object Advent23 {
-  type Input = Field2D[Square]
+  type Part1Input = Field2D[Square]
 
   sealed trait Square extends Product with Serializable
   object Square {
@@ -20,7 +23,7 @@ object Advent23 {
     final case class Slope(direction: CardinalDirection2D) extends Square
   }
 
-  def parse(input: String): Input =
+  def parse(input: String): Part1Input =
     Field2D.parseFromBiMap(input, BiMap(
       '#' <-> Forest,
       '.' <-> Path,
@@ -31,11 +34,11 @@ object Advent23 {
     ))
 
   final case class State(
-    visited: Set[Coords2D],
+    visited: List[Coords2D],
     steps: Int,
     current: Coords2D,
   ) {
-    def next(field: Input): List[State] = {
+    def next(field: Part1Input): List[State] = {
       val candidates = field.atOrElse(current, Forest) match {
         case Square.Forest => "wtf".fail
         case Square.Path => field.adjacent4(current)
@@ -52,7 +55,7 @@ object Advent23 {
 
       valid map { c =>
         State(
-          visited = visited + c,
+          visited = c :: visited,
           steps = steps + 1,
           current = c
         )
@@ -60,12 +63,12 @@ object Advent23 {
     }
   }
 
-  def solve1(data: Input): Int = {
+  def solve1(data: Part1Input): Int = {
     val startCoords = data.topRowCoords.find(c => data.at(c).contains(Path)).get
     val goalCoords = data.bottomRowCoords.find(c => data.at(c).contains(Path)).get
 
     val startState = State(
-      visited = Set(startCoords),
+      visited = List(startCoords),
       steps = 0,
       current = startCoords,
     )
@@ -75,12 +78,16 @@ object Advent23 {
       _.next(data),
     )
 
-    reachable.filter(_.current == goalCoords).maxBy(_.steps).steps
+    val best = reachable.filter(_.current == goalCoords).maxBy(_.steps)
+    println(best.visited.length)
+    assert(best.visited.allDistinct)
+    best.visited.reverse.foreach(println)
+    best.steps
   }
 
-  def part1(data: Input): Long = solve1(data)
+  def part1(data: Part1Input): Long = solve1(data)
 
-  def part2(data: Input): Long = {
+  def part2Crude(data: Part1Input): Long = {
     val updated = data.map {
       case Square.Forest => Square.Forest
       case Square.Path => Square.Path
@@ -90,14 +97,170 @@ object Advent23 {
     solve1(updated)
   }
 
-  def parseFile(fileName: String): Input =
+  final case class Edge(vertices: SetOfTwo[VertexId], distance: Long) {
+    def other(vertexId: VertexId): VertexId = {
+      (vertices.underlying - vertexId).toList.singleResultUnsafe
+    }
+  }
+
+  type VertexId = Int
+  final case class UndirectedGraph[L](
+    labelToIndexMap: BiMap[L, VertexId],
+    edges: Set[Edge],
+  ) {
+    def areConnected(a: VertexId, b: VertexId): Option[Edge] = edges.find(_.vertices == SetOfTwo(a, b))
+    def edgesFor(vertexId: VertexId): Set[Edge] = edges.filter(_.vertices.contains(vertexId))
+    def connectedTo(vertexId: VertexId): Seq[VertexId] = edges.filter(_.vertices.contains(vertexId)).toList.map(_.other(vertexId))
+
+    def connectors: Iterable[VertexId] = labelToIndexMap.rightKeys.filter(v => edgesFor(v).size > 2)
+  }
+
+  private def printDot(graph: UndirectedGraph[Coords2D], start: VertexId, goal: VertexId): Unit = {
+    def vertexName(v: VertexId): String = {
+      val c = graph.labelToIndexMap.rightToLeftUnsafe(v)
+      s"x${c.x}y${c.y}"
+    }
+
+    println("graph G {")
+    println()
+    println(s"""${vertexName(start)} [color="green"]""")
+    println(s"""${vertexName(goal)} [color="red"]""")
+
+    graph.edges.foreach { e =>
+      val (a, b) = e.vertices.tupleInArbitraryOrder
+      println(s"""${vertexName(a)} -- ${vertexName(b)} [ label="${e.distance}" ]""")
+    }
+
+    println("}")
+    println()
+  }
+
+  def simplifyGraph(graph: UndirectedGraph[Coords2D], start: VertexId, goal: VertexId): UndirectedGraph[Coords2D] = {
+    val connectors = graph.connectors.toSet + start + goal
+    println(connectors.size)
+
+    println(connectors.map(graph.labelToIndexMap.rightToLeftUnsafe))
+
+    var newEdges = Set.empty[Edge]
+
+    connectors foreach { connector =>
+      def helper(v: VertexId) = if (v == connector || !connectors.contains(v)) {
+        graph.connectedTo(v).toList
+      } else {
+        Nil
+      }
+
+      val reachable = Bfs.bfsReachable[VertexId](connector, helper)
+
+      reachable.filterNot(_ == connector) foreach { n =>
+        if (connectors.contains(n)) {
+          val distance = Bfs.bfsLength[VertexId](connector, helper, _ == n).get
+          val edge = Edge(SetOfTwo(connector, n), distance)
+          newEdges += edge
+        }
+      }
+    }
+
+    val labelToIndexMap = connectors.map { connector =>
+      graph.labelToIndexMap.rightToLeftUnsafe(connector) -> connector
+    }
+
+    UndirectedGraph[Coords2D](
+      labelToIndexMap = BiMap.from(labelToIndexMap),
+      edges = newEdges,
+    )
+  }
+
+  final case class State2(
+    visited: Set[VertexId],
+    steps: Long,
+    current: VertexId,
+  ) {
+    def next(graph: UndirectedGraph[Coords2D]): List[State2] = {
+      graph.edgesFor(current).flatMap { e =>
+        val other = e.other(current)
+        if (visited.contains(other)) {
+          Nil
+        } else {
+          State2(
+            visited = visited + other,
+            steps = steps + e.distance,
+            current = other,
+          ) :: Nil
+        }
+      }.toList
+    }
+  }
+
+  def solve2(graph: UndirectedGraph[Coords2D], start: VertexId, goal: VertexId): Long = {
+    val simplified = simplifyGraph(graph, start, goal)
+
+    printDot(simplified, start, goal)
+
+    val startState = State2(
+      visited = Set(start),
+      steps = 0,
+      current = start,
+    )
+
+    var best = 0L
+    Bfs.bfsVisitAll[State2](
+      startState,
+      _.next(simplified),
+      state => {
+        if (state.current == goal) {
+          if (state.steps > best) {
+            best = state.steps
+            println(s"$best: $state")
+          }
+        }
+      }
+    )
+    best
+  }
+
+  def part2(data: Part1Input): Long = {
+    val field = data.map {
+      case Square.Forest => Square.Forest
+      case Square.Path => Square.Path
+      case Slope(_) => Square.Path
+    }
+
+    val startCoords = field.topRowCoords.find(c => field.at(c).contains(Path)).get
+    val goalCoords = field.bottomRowCoords.find(c => field.at(c).contains(Path)).get
+
+    var nextIndex = 0
+    var labelToIndexMap: Map[Coords2D, VertexId] = Map.empty
+    field.allCoords.foreach { c =>
+      field.at(c).foreach {
+        case Square.Forest =>
+        case _ =>
+          labelToIndexMap = labelToIndexMap + (c -> nextIndex)
+          nextIndex += 1
+      }
+    }
+
+    var edges: Set[Edge] = Set.empty
+
+    field.allConnectionsDirectional foreach { case (a, _, b) =>
+      if (field.at(a).contains(Path) && field.at(b).contains(Path)) {
+        val vertices = SetOfTwo(labelToIndexMap(a), labelToIndexMap(b))
+        edges += Edge(vertices, 1)
+      }
+    }
+
+    val graph = UndirectedGraph(BiMap.from(labelToIndexMap), edges)
+    solve2(graph, labelToIndexMap(startCoords), labelToIndexMap(goalCoords))
+  }
+
+  def parseFile(fileName: String): Part1Input =
     parse(readFileText(fileName))
 
   def fileName(suffix: String): String =
     s"2023/23$suffix.txt"
 
   def main(args: Array[String]): Unit = {
-    val realData: Input = parseFile(fileName(""))
+    val realData: Part1Input = parseFile(fileName(""))
 
     println(s"Part 1: ${part1(realData)}")
     println(s"Part 2: ${part2(realData)}")
--- scala2/src/test/scala/jurisk/adventofcode/y2023/Advent23Spec.scala
+++ scala2/src/test/scala/jurisk/adventofcode/y2023/Advent23Spec.scala
@@ -6,6 +6,7 @@ import org.scalatest.matchers.should.Matchers._
 
 class Advent23Spec extends AnyFreeSpec {
   private def testData = parseFile(fileName("-test"))
+  private def simpleData = parseFile(fileName("-test-simple"))
   private def realData = parseFile(fileName(""))
 
   "part 1" - {
@@ -19,6 +20,14 @@ class Advent23Spec extends AnyFreeSpec {
   }
 
   "part 2" - {
+    "test simple" in {
+      part2Crude(simpleData) shouldEqual part2(simpleData)
+    }
+
+    "test crude" in {
+      part2Crude(testData) shouldEqual 154
+    }
+
     "test" in {
       part2(testData) shouldEqual 154
     }
