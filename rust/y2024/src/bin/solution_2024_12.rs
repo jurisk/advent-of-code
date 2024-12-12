6a4db9611bea604dfdb3dd5e8e4788a5d355613d
jurisk <4895679+jurisk@users.noreply.github.com>
2024-12-12 21:42:00 +0200

    2024-12 (#30)

---
+++ rust/y2024/src/bin/solution_2024_12.rs
@@ -0,0 +1,173 @@
+use std::collections::HashSet;
+use std::convert::identity;
+
+use advent_of_code_common::direction::Direction;
+use advent_of_code_common::grid2d::{Coords, Grid2D, MatrixGrid2D};
+use advent_of_code_common::itertools::group_by::GroupBy;
+use itertools::Itertools;
+use pathfinding::prelude::connected_components;
+
+const DATA: &str = include_str!("../../resources/12.txt");
+
+type R = usize;
+type Data = MatrixGrid2D<char>;
+
+fn parse(input: &str) -> Data {
+    MatrixGrid2D::parse(input, identity)
+}
+
+fn perimeter(coords: &HashSet<Coords>) -> R {
+    coords
+        .iter()
+        .map(|c| {
+            c.adjacent4()
+                .into_iter()
+                .filter(|n| !coords.contains(n))
+                .count()
+        })
+        .sum()
+}
+
+fn ascending_sequences(data: Vec<i32>) -> usize {
+    let mut data = data;
+    data.sort_unstable();
+    if data.is_empty() {
+        0
+    } else if data.len() == 1 {
+        1
+    } else {
+        data.iter()
+            .tuple_windows()
+            .filter(|&(a, b)| a + 1 != *b)
+            .count()
+            + 1
+    }
+}
+
+fn number_of_sides(garden_coords: &HashSet<Coords>) -> R {
+    Direction::all()
+        .into_iter()
+        .map(|d| {
+            garden_coords
+                .iter()
+                .filter(|&c| !garden_coords.contains(&(*c + d)))
+                .group_by_key(|c| {
+                    match d {
+                        Direction::North | Direction::South => c.y,
+                        Direction::East | Direction::West => c.x,
+                    }
+                })
+                .values()
+                .map(|coords| {
+                    let values = coords
+                        .iter()
+                        .map(|c| {
+                            match d {
+                                Direction::North | Direction::South => c.x,
+                                Direction::East | Direction::West => c.y,
+                            }
+                        })
+                        .collect();
+                    ascending_sequences(values)
+                })
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn solve<F>(data: &Data, f: F) -> R
+where
+    F: Fn(&HashSet<Coords>) -> R,
+{
+    let coords: Vec<_> = data.coords().collect();
+    let regions = connected_components(&coords, |&c| {
+        data.neighbours_for(c, false)
+            .into_iter()
+            .filter(move |&n| data.get(n) == data.get(c))
+    });
+
+    regions
+        .into_iter()
+        .map(|coords| {
+            let area = coords.len();
+            area * f(&coords)
+        })
+        .sum()
+}
+
+fn solve_1(data: &Data) -> R {
+    solve(data, perimeter)
+}
+
+fn solve_2(data: &Data) -> R {
+    solve(data, number_of_sides)
+}
+
+fn main() {
+    let data = parse(DATA);
+
+    let result_1 = solve_1(&data);
+    println!("Part 1: {result_1}");
+
+    let result_2 = solve_2(&data);
+    println!("Part 2: {result_2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn real_data() -> Data {
+        parse(DATA)
+    }
+
+    #[test]
+    fn test_solve_1_real() {
+        assert_eq!(solve_1(&real_data()), 1_371_306);
+    }
+
+    #[test]
+    fn test_solve_2_test_0() {
+        assert_eq!(
+            solve_2(&parse(include_str!("../../resources/12-test-00.txt"))),
+            80
+        );
+    }
+
+    #[test]
+    fn test_solve_2_test_1() {
+        assert_eq!(
+            solve_2(&parse(include_str!("../../resources/12-test-01.txt"))),
+            436
+        );
+    }
+
+    #[test]
+    fn test_solve_2_test_2() {
+        assert_eq!(
+            solve_2(&parse(include_str!("../../resources/12-test-02.txt"))),
+            1206
+        );
+    }
+
+    #[test]
+    fn test_solve_2_test_3() {
+        assert_eq!(
+            solve_2(&parse(include_str!("../../resources/12-test-03.txt"))),
+            236
+        );
+    }
+
+    #[test]
+    fn test_solve_2_test_4() {
+        assert_eq!(
+            solve_2(&parse(include_str!("../../resources/12-test-04.txt"))),
+            368
+        );
+    }
+
+    #[test]
+    fn test_solve_2_real() {
+        assert_eq!(solve_2(&real_data()), 805_880);
+    }
+}
--- scala2/src/test/scala/jurisk/adventofcode/y2024/Advent12Spec.scala
+++ scala2/src/test/scala/jurisk/adventofcode/y2024/Advent12Spec.scala
@@ -5,26 +5,54 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers._
 
 class Advent12Spec extends AnyFreeSpec {
-  private def testData = parseFile(fileName("-test-00"))
-  private def realData = parseFile(fileName(""))
+  private def testData0 = parseFile(fileName("-test-00"))
+  private def testData1 = parseFile(fileName("-test-01"))
+  private def testData2 = parseFile(fileName("-test-02"))
+  private def testData3 = parseFile(fileName("-test-03"))
+  private def testData4 = parseFile(fileName("-test-04"))
+  private def realData  = parseFile(fileName(""))
 
   "part 1" - {
-    "test" in {
-      part1(testData) shouldEqual 0
+    "test 0" in {
+      part1(testData0) shouldEqual 140
+    }
+
+    "test 1" in {
+      part1(testData1) shouldEqual 772
+    }
+
+    "test 2" in {
+      part1(testData2) shouldEqual 1930
     }
 
     "real" in {
-      part1(realData) shouldEqual 0
+      part1(realData) shouldEqual 1371306
     }
   }
 
   "part 2" - {
-    "test" in {
-      part2(testData) shouldEqual 0
+    "test 0" in {
+      part2(testData0) shouldEqual 80
+    }
+
+    "test 1" in {
+      part2(testData1) shouldEqual 436
+    }
+
+    "test 3" in {
+      part2(testData3) shouldEqual 236
+    }
+
+    "test 4" in {
+      part2(testData4) shouldEqual 368
+    }
+
+    "test 2" in {
+      part2(testData2) shouldEqual 1206
     }
 
     "real" in {
-      part2(realData) shouldEqual 0
+      part2(realData) shouldEqual 805880
     }
   }
 }
