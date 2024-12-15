881662bda154ceefe0e671f8da80080b586d9a64
Juris <jurisk@users.noreply.github.com>
2024-12-15 07:35:11 +0200

    2024-15 Scala 2 WIP

--- scala2/src/main/scala/jurisk/adventofcode/y2024/Advent15.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2024/Advent15.scala
@@ -1,34 +1,116 @@
 package jurisk.adventofcode.y2024
 
+import cats.implicits.toFunctorOps
+import jurisk.geometry.Direction2D.{CardinalDirection2D, parseCaretToOption}
+import jurisk.geometry.{Coords2D, Field2D}
 import jurisk.utils.FileInput._
 import jurisk.utils.Parsing.StringOps
 
+import scala.annotation.tailrec
+
 object Advent15 {
-  type Input = List[Command]
-  type N     = Long
-
-  sealed trait Command extends Product with Serializable
-  object Command {
-    case object Noop                      extends Command
-    final case class Something(
-      values: List[N]
-    ) extends Command
-    final case class Other(value: String) extends Command
-
-    def parse(s: String): Command =
-      s match {
-        case "noop"            => Noop
-        case s"something $rem" => Something(rem.extractLongList)
-        case s if s.nonEmpty   => Other(s)
-        case _                 => s.failedToParse
+  sealed trait Square extends Product with Serializable {
+    def toChar: Char =
+      this match {
+        case Square.Empty => '.'
+        case Square.Wall  => '#'
+        case Square.Box   => 'O'
       }
   }
+  object Square {
+    final case object Empty extends Square
+    final case object Wall  extends Square
+    final case object Box   extends Square
+  }
 
-  def parse(input: String): Input =
-    input.parseLines(Command.parse)
+  final case class State(
+    robot: Coords2D,
+    field: Field2D[Square],
+  ) {
+    def print(legend: String): Unit = {
+      val charField = field.map(_.toChar)
+      val f         = charField.updatedAtUnsafe(robot, '@')
+      println(legend)
+      Field2D.printCharField(f)
+    }
 
-  def part1(data: Input): N =
-    0
+    @tailrec
+    private def calculateMovePackage(
+      c: Coords2D,
+      direction: CardinalDirection2D,
+      acc: List[Coords2D] = Nil,
+    ): List[Coords2D] = {
+      val next = c + direction
+      field.at(next) match {
+        case Some(Square.Empty) => acc.reverse
+        case Some(Square.Wall)  => Nil
+        case Some(Square.Box)   =>
+          calculateMovePackage(next, direction, next :: acc)
+        case None               => sys.error("unexpected")
+      }
+    }
+
+    private def moveRobot(next: Coords2D): State =
+      State(next, field)
+
+    def move(direction: CardinalDirection2D): State = {
+      val next        = robot + direction
+      val movePackage = calculateMovePackage(robot, direction)
+      movePackage match {
+        case Nil =>
+          if (field.at(next).contains(Square.Empty)) {
+            moveRobot(next)
+          } else {
+            this
+          }
+        case _   =>
+          val h         = movePackage.head
+          val t         = movePackage.last
+          val t_next    = t + direction
+          val nextField = field
+            .updatedAtUnsafe(t_next, Square.Box)
+            .updatedAtUnsafe(h, Square.Empty)
+          moveRobot(next).copy(field = nextField)
+      }
+    }
+  }
+  type Input = (State, List[CardinalDirection2D])
+  type N = Long
+
+  def parse(input: String): Input = {
+    val (a, b)     = input.splitPairByDoubleNewline
+    val charField  = Field2D.parseCharField(a)
+    val robot      = charField.findCoordsByValue('@').get
+    val field      = charField.map {
+      case '.' => Square.Empty
+      case '#' => Square.Wall
+      case '@' => Square.Empty
+      case 'O' => Square.Box
+    }
+    val state      = State(robot, field)
+    val dirs       = b.splitLines.mkString
+    val directions = dirs.map(parseCaretToOption(_).get).toList
+    (state, directions)
+  }
+
+  def part1(data: Input): N = {
+    val (state, directions) = data
+    println(s"state: $state, directions: $directions")
+    state.print("Initial state:")
+    val result              = directions.foldLeft(state) { (state, direction) =>
+      val r = state.move(direction)
+      r.print(s"Move $direction:")
+      r
+    }
+    result.print("Final state:")
+    result.field.allCoords.map { c =>
+      if (result.field.at(c).contains(Square.Box)) {
+        100 * c.y + c.x
+      } else {
+        0
+      }
+    }.sum
+  }
 
   def part2(data: Input): N =
     0
--- scala2/src/test/scala/jurisk/adventofcode/y2024/Advent15Spec.scala
+++ scala2/src/test/scala/jurisk/adventofcode/y2024/Advent15Spec.scala
@@ -5,22 +5,27 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers._
 
 class Advent15Spec extends AnyFreeSpec {
-  private def testData = parseFile(fileName("-test-00"))
-  private def realData = parseFile(fileName(""))
+  private def testData0 = parseFile(fileName("-test-00"))
+  private def testData1 = parseFile(fileName("-test-01"))
+  private def realData  = parseFile(fileName(""))
 
   "part 1" - {
-    "test" in {
-      part1(testData) shouldEqual 0
+    "test 0" in {
+      part1(testData0) shouldEqual 2028
+    }
+
+    "test 1" in {
+      part1(testData1) shouldEqual 10092
     }
 
     "real" in {
-      part1(realData) shouldEqual 0
+      part1(realData) shouldEqual 1577255
     }
   }
 
   "part 2" - {
     "test" in {
-      part2(testData) shouldEqual 0
+      part2(testData1) shouldEqual 9021
     }
 
     "real" in {
