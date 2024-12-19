1bde9ace92aa1bf51d46572d43bec07e5863eba9
Juris <jurisk@users.noreply.github.com>
2024-12-19 07:11:03 +0200

    2024-19 Scala 2 WIP

--- scala2/src/main/scala/jurisk/adventofcode/y2024/Advent19.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2024/Advent19.scala
@@ -4,31 +4,33 @@ import jurisk.utils.FileInput._
 import jurisk.utils.Parsing.StringOps
 
 object Advent19 {
-  type Input = List[Command]
-  type N     = Long
+  final case class Input(
+    stripes: List[String],
+    towels: List[String],
+  )
+  type N     = Int
 
-  sealed trait Command extends Product with Serializable
-  object Command {
-    case object Noop                      extends Command
-    final case class Something(
-      values: List[N]
-    ) extends Command
-    final case class Other(value: String) extends Command
-
-    def parse(s: String): Command =
-      s match {
-        case "noop"            => Noop
-        case s"something $rem" => Something(rem.extractLongList)
-        case s if s.nonEmpty   => Other(s)
-        case _                 => s.failedToParse
-      }
+  def parse(input: String): Input = {
+    val (stripes, towels) = input.split("\n\n").toList match {
+      case List(stripes, towels) => (stripes.split(", ").toList, towels.linesIterator.toList)
+      case _ => throw new Exception("Invalid input")
+    }
+    Input(stripes, towels)
   }
 
-  def parse(input: String): Input =
-    input.parseLines(Command.parse)
+  private def isValid(towel: String, stripes: List[String]): Boolean = {
+    if (towel.isEmpty) true
+    else {
+      stripes.exists { stripe =>
+//        println(s"Towel $towel, Stripe $stripe, Starts: ${towel.startsWith(stripe)}")
+        towel.startsWith(stripe) && isValid(towel.drop(stripe.length), stripes)
+      }
+    }
+  }
 
-  def part1(data: Input): N =
-    0
+  def part1(data: Input): N = {
+    data.towels.count(towel => isValid(towel, data.stripes))
+  }
 
   def part2(data: Input): N =
     0
