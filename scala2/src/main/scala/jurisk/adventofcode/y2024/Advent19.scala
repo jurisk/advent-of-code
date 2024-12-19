a71e52afa8892595fcbf28cea03673c989feeb46
Juris <jurisk@users.noreply.github.com>
2024-12-19 07:19:16 +0200

    2024-19 Scala 2

--- scala2/src/main/scala/jurisk/adventofcode/y2024/Advent19.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2024/Advent19.scala
@@ -2,44 +2,27 @@ package jurisk.adventofcode.y2024
 
 import jurisk.utils.FileInput._
 import jurisk.utils.Memoize
-import jurisk.utils.Parsing.StringOps
 
 object Advent19 {
   final case class Input(
     stripes: List[String],
     towels: List[String],
   )
-  type N     = Long
+  type N = Long
 
   def parse(input: String): Input = {
     val (stripes, towels) = input.split("\n\n").toList match {
-      case List(stripes, towels) => (stripes.split(", ").toList, towels.linesIterator.toList)
-      case _ => throw new Exception("Invalid input")
+      case List(stripes, towels) =>
+        (stripes.split(", ").toList, towels.linesIterator.toList)
+      case _                     => throw new Exception("Invalid input")
     }
     Input(stripes, towels)
   }
 
-  def part1(data: Input): N = {
-    def ways(towel: String, stripes: List[String]): N = {
-      if (towel.isEmpty) 1L
-      else {
-        stripes.map { stripe =>
-          if (towel.startsWith(stripe)) {
-            ways(towel.drop(stripe.length), stripes)
-          } else {
-            0L
-          }
-        }.sum
-      }
-    }
-
-    data.towels.count(towel => ways(towel, data.stripes) > 0)
-  }
-
-  def part2(data: Input): N = {
+  def solve(data: Input, countF: N => N): N = {
     lazy val waysMemoized = Memoize.memoize1(ways)
 
-    def ways(towel: String): N = {
+    def ways(towel: String): N =
       if (towel.isEmpty) 1L
       else {
         data.stripes.map { stripe =>
@@ -50,10 +33,15 @@ object Advent19 {
           }
         }.sum
       }
-    }
 
-    data.towels.map(towel => waysMemoized(towel)).sum
+    data.towels.map(towel => countF(waysMemoized(towel))).sum
   }
+
+  def part1(data: Input): N =
+    solve(data, _ min 1)
+
+  def part2(data: Input): N =
+    solve(data, identity)
 
   def parseFile(fileName: String): Input =
     parse(readFileText(fileName))
