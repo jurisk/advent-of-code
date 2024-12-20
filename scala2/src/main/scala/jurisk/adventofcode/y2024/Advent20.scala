819576764628e8f56dabc9b12417ea6f069a7513
jurisk <4895679+jurisk@users.noreply.github.com>
2024-12-20 19:34:23 +0200

    2024-20 (#36)

--- scala2/src/main/scala/jurisk/adventofcode/y2024/Advent20.scala
+++ scala2/src/main/scala/jurisk/adventofcode/y2024/Advent20.scala
@@ -1,37 +1,99 @@
 package jurisk.adventofcode.y2024
 
+import jurisk.algorithms.pathfinding.Dijkstra
+import jurisk.geometry.Coords2D
+import jurisk.geometry.Field2D
 import jurisk.utils.FileInput._
 import jurisk.utils.Parsing.StringOps
 
 object Advent20 {
-  type Input = List[Command]
+  type Input = (Coords2D, Field2D[Boolean], Coords2D)
   type N     = Long
 
-  sealed trait Command extends Product with Serializable
-  object Command {
-    case object Noop                      extends Command
-    final case class Something(
-      values: List[N]
-    ) extends Command
-    final case class Other(value: String) extends Command
-
-    def parse(s: String): Command =
-      s match {
-        case "noop"            => Noop
-        case s"something $rem" => Something(rem.extractLongList)
-        case s if s.nonEmpty   => Other(s)
-        case _                 => s.failedToParse
-      }
+  final case class Cheat(from: Coords2D, to: Coords2D) {
+    def distance: Int = from manhattanDistance to
   }
 
-  def parse(input: String): Input =
-    input.parseLines(Command.parse)
+  def parse(input: String): Input = {
+    val charField = Field2D.parseCharField(input)
+    val start     =
+      charField.findCoordsByValue('S').getOrElse("Start not found".fail)
+    val end       = charField.findCoordsByValue('E').getOrElse("End not found".fail)
+    val field     = charField.mapByCoordsWithValues { case (_, c) =>
+      c match {
+        case 'S' => false
+        case 'E' => false
+        case '#' => true
+        case '.' => false
+        case _   => s"Unknown character $c in field".fail
+      }
+    }
+    (start, field, end)
+  }
 
-  def part1(data: Input): N =
-    0
+  def solve(data: Input, saveAtLeast: Int, maxCheat: Int): N = {
+    val (start, field, end) = data
 
-  def part2(data: Input): N =
-    0
+    def successors(c: Coords2D): List[(Coords2D, Int)] =
+      field
+        .neighboursFor(c, includeDiagonal = false)
+        .filter(field.at(_).contains(false))
+        .map { n =>
+          (n, 1)
+        }
+
+    val fromStart = Dijkstra.dijkstraAll[Coords2D, Int](
+      start,
+      successors,
+    )
+
+    val fromEnd = Dijkstra.dijkstraAll[Coords2D, Int](
+      end,
+      successors,
+    )
+
+    val (_, cost)         = fromStart.getOrElse(end, "No path from start to end".fail)
+    val goalCostThreshold = cost - saveAtLeast
+
+    println(s"Without cheats = $cost, goal cost threshold = $goalCostThreshold")
+
+    def validCheat(cheat: Cheat): Boolean =
+      (fromStart.get(cheat.from), fromEnd.get(cheat.to)) match {
+        case (Some((_, startCost)), Some((_, endCost))) =>
+          startCost + endCost + cheat.distance <= goalCostThreshold
+        case _                                          =>
+          false
+      }
+
+    val empties = field.allCoords
+      .filter(field.at(_).contains(false))
+    val cheats  =
+      for {
+        // Could be optimised by avoiding processing all O(N^2), as if we know one coordinate, we can generate all others within maxCheat distance more efficiently
+        c1 <- empties
+        c2 <- empties
+        if c1.manhattanDistance(c2) <= maxCheat
+        if c1 != c2
+      } yield Cheat(c1, c2)
+
+    println(s"Cheats: ${cheats.size}")
+
+    val selectedCheats =
+      cheats
+        .filter(validCheat)
+        .toSet
+
+    println(s"Selected cheats: ${selectedCheats.size}")
+    selectedCheats.foreach(println)
+
+    selectedCheats.size
+  }
+
+  def part1(data: Input, saveAtLeast: Int): N =
+    solve(data, saveAtLeast, 2)
+
+  def part2(data: Input, saveAtLeast: Int): N =
+    solve(data, saveAtLeast, 20)
 
   def parseFile(fileName: String): Input =
     parse(readFileText(fileName))
@@ -42,7 +104,7 @@ object Advent20 {
   def main(args: Array[String]): Unit = {
     val realData: Input = parseFile(fileName(""))
 
-    println(s"Part 1: ${part1(realData)}")
-    println(s"Part 2: ${part2(realData)}")
+    println(s"Part 1: ${part1(realData, 100)}")
+    println(s"Part 2: ${part2(realData, 100)}")
   }
 }
--- scala2/src/test/scala/jurisk/adventofcode/y2024/Advent20Spec.scala
+++ scala2/src/test/scala/jurisk/adventofcode/y2024/Advent20Spec.scala
@@ -9,22 +9,38 @@ class Advent20Spec extends AnyFreeSpec {
   private def realData = parseFile(fileName(""))
 
   "part 1" - {
-    "test" in {
-      part1(testData) shouldEqual 0
+    "test 64" in {
+      part1(testData, 64) shouldEqual 1
     }
 
-    "real" in {
-      part1(realData) shouldEqual 0
+    "test 40" in {
+      part1(testData, 40) shouldEqual 1 + 1
+    }
+
+    "test misc" in {
+      part1(testData, 38) shouldEqual 1 + 1 + 1
+      part1(testData, 36) shouldEqual 1 + 1 + 1 + 1
+      part1(testData, 20) shouldEqual 1 + 1 + 1 + 1 + 1
+      part1(testData, 12) shouldEqual 1 + 1 + 1 + 1 + 1 + 3
+      part1(testData, 10) shouldEqual 1 + 1 + 1 + 1 + 1 + 3 + 2
+      part1(testData, 8) shouldEqual 1 + 1 + 1 + 1 + 1 + 3 + 2 + 4
+      part1(testData, 6) shouldEqual 1 + 1 + 1 + 1 + 1 + 3 + 2 + 4 + 2
+      part1(testData, 4) shouldEqual 1 + 1 + 1 + 1 + 1 + 3 + 2 + 4 + 2 + 14
+      part1(testData, 2) shouldEqual 1 + 1 + 1 + 1 + 1 + 3 + 2 + 4 + 2 + 14 + 14
+    }
+
+    "real" ignore {
+      part1(realData, 100) shouldEqual 1293
     }
   }
 
   "part 2" - {
     "test" in {
-      part2(testData) shouldEqual 0
+      part2(testData, 76) shouldEqual 3
     }
 
-    "real" in {
-      part2(realData) shouldEqual 0
+    "real" ignore {
+      part2(realData, 100) shouldEqual 977747
     }
   }
 }
